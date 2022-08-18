8c35410c66d33d67a5e8eecb6e2b63bf7db42fd8
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-18 09:09:24 -0700

    books

--- server/Schema/schema.js
+++ server/Schema/schema.js
@@ -4,12 +4,16 @@ const {GraphQLObjectType,
     GraphQLString,
     GraphQLID,
     GraphQLInt,
+    GraphQLList,
     GraphQLSchema}=graphql
-
+//dummay data
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1',authorId:'1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2',authorId:'2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3',authorId:'3' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 var authors = [
@@ -25,7 +29,14 @@ const BookType= new GraphQLObjectType({
     fields:()=>({
         id:{type:GraphQLID},
         name:{type:GraphQLString},
-        genre:{type:GraphQLString}
+        genre:{type:GraphQLString},
+        author:{
+            type:AuthorType,
+            resolve(parent,args){
+              
+               return _.find(authors, {id:parent.authorId})
+            }
+        }
     })
 })
 
@@ -35,8 +46,15 @@ const AuthorType= new GraphQLObjectType({
     fields:()=>({
         id:{type:GraphQLID},
         name:{type:GraphQLString},
-        age:{type:graphql.GraphQLInt}
-    })
+        age:{type:graphql.GraphQLInt},
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                console.log(parent.id)
+                return _.filter(books,{authorId:parent.id})
+            }
+        }
+    }),
 })
 
 const RootQuery=new GraphQLObjectType({
@@ -59,6 +77,7 @@ const RootQuery=new GraphQLObjectType({
             }
         }
     }
+    
 })
 
 module.exports=new GraphQLSchema({
